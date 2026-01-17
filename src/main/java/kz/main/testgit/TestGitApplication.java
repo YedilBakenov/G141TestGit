5691c885be956c458c5a4ebfef07d7d20f025375
yedil.bakenov <e.bakenov@kpecom.kz>
2026-01-17 12:51:18 +0500

    testGit: Добавил весь проект на github

---
+++ src/main/java/kz/main/testgit/TestGitApplication.java
@@ -0,0 +1,13 @@
+package kz.main.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitApplication.class, args);
+    }
+
+}
